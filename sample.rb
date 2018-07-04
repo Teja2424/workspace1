f558fa563bf17475fbf0a3c811a4ddc9f94371bf
Teja2424 <venkatateja242@gmail.com>
2018-07-04 09:36:18 -0700

    first

---
+++ sample.rb
@@ -0,0 +1,7 @@
+class Sample1
+def Sample
+yield
+yield
+yield
+end
+Sample {puts"hello world"}
---
+++ self.rb
@@ -0,0 +1,11 @@
+class Person
+def self.person(name,add,email)
+@n=name
+@a=address
+@e=email
+puts "the person name is#@n"
+puts "the person address is#@a"
+puts "the person emai is#@e"
+end
+end
+Person.sample("sai","bang","sai@gmail.com")
