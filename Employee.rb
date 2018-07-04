86cb8395a68d1b2bcaa6303ed29a9e12189322bd
Teja2424 <venkatateja242@gmail.com>
2018-07-03 20:56:11 -0700

    first

--- Employee.rb
+++ Employee.rb
@@ -4,7 +4,7 @@ def initialize (name,payrate)
  @payrate=payrate
  end
 def name
-puts "the employee name is :#@name"
+puts "the employee name is:#@name"
 end
 def pay(hours)
 return @payrate*hours
@@ -15,3 +15,7 @@ def initialize (name,payrate)
 super(name,payrate)
 end
 end
+E= employee.new("sai",200)
+puts(e.name,"pay: ",e.pay(120))
+M=manager.new("kartik",1200)
+print(m.name,"pay:",m.pay(60))
